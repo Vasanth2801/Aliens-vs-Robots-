219db9450b3f71493f21aa8f51da1350b7a163bc
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-03-10 07:55:47 +0530

    Backgroound Scroller

--- Aliens vs Robots/Assets/Scripts/Enemy/EnemyDestroyer.cs
+++ Aliens vs Robots/Assets/Scripts/Enemy/EnemyDestroyer.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class EnemyDestroyer : MonoBehaviour
 {
+    [SerializeField] private GameObject gameOver;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Enemy")
         {
             collision.gameObject.SetActive(false);
+            gameOver.SetActive(true);
         }
     }
 }
